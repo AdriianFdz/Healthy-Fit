ea6360fced250f8ed73c397bcad95d792a432544
Adrian Fernandez <adrian.f@opendeusto.es>
2023-10-10 21:34:21 +0200

    Creada ventana resumen y paquete gui

---
+++ HealthyAndFit/src/gui/VentanaResumen.java
@@ -0,0 +1,19 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class VentanaResumen extends JFrame{
+	public VentanaResumen() {
+		
+		
+		
+		
+		
+		
+		
+		setSize(1920,1080);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setTitle("Resumen");
+	}
+}
