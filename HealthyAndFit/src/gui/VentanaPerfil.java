c8176971f5e02a5d759c66000155bfa0cca8fd19
Iker Alvis <iker.a@opendeusto.es>
2023-10-12 21:38:09 +0200

    Seguir con ventana (2)

--- HealthyAndFit/src/gui/VentanaPerfil.java
+++ HealthyAndFit/src/gui/VentanaPerfil.java
@@ -1,6 +1,9 @@
 package gui;
 
 import java.awt.GridLayout;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
 
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
@@ -9,12 +12,15 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
+import domain.TipoEnfermedades;
+import domain.TipoPreferencia;
 import domain.Usuario;
 
 
+
 public class VentanaPerfil extends JFrame{
-	
 	JPanel panel1;
 	JPanel panelColum1;
 	JLabel labelNombre;
@@ -33,6 +39,8 @@ public class VentanaPerfil extends JFrame{
 	JLabel labelCorreo;
 	JLabel labelPeso;
 	JLabel labelIMC;
+	JLabel labelPrefAli;
+	JLabel labelAleg;
 	
 	public VentanaPerfil(Usuario p) {
 		panel1 = new JPanel(new GridLayout(1, 3));
@@ -67,33 +75,54 @@ public class VentanaPerfil extends JFrame{
 		panelColum2 = new JPanel();
 		panelColum2.setLayout(new BoxLayout(panelColum2, BoxLayout.Y_AXIS));
 
-		labelEdad = new JLabel("Edad");
-		labelSexo = new JLabel("Sexo");
-		labelAltura = new JLabel("Altura");
+		//Problema con la fecha, da una exception debido al tipo de datos
+		LocalDate ahora = LocalDate.now();
+		Period periodo = Period.between(LocalDate.of(p.getfechaNacimiento().getYear(), p.getfechaNacimiento().getHours(), p.getfechaNacimiento().getMinutes()), ahora);
+		
+		labelEdad = new JLabel(periodo.toString());
+		labelSexo = new JLabel(p.getSexo().name());
+		labelAltura = new JLabel(Double.toString(p.getAltura()));
 		labelEnfer = new JLabel("Enfermedades");
 
-		JComboBox<String> enfermedadesComboBox = new JComboBox<>();
+		JScrollPane paneEnfermedades = new JScrollPane();
+		//for (TipoEnfermedades p.getEnfermedades() : enfermedades) {
 
+
+		labelPrefAli = new JLabel("PREFERENCIAS ALIMENTICIAS");
+		JComboBox<TipoPreferencia> preferenciasComboBox = new JComboBox<TipoPreferencia>();
+		//for (TipoPreferencia preferenciaAlimenticia : ) {
+			
+		//}
+		
 		panelColum2.add(labelEdad);
 		panelColum2.add(labelSexo);
 		panelColum2.add(labelAltura);
-
+		panelColum2.add(paneEnfermedades);
+		panelColum2.add(preferenciasComboBox);
+		
 		panel1.add(panelColum2);
 
 		panelColum3 = new JPanel();
 		panelColum3.setLayout(new BoxLayout(panelColum3, BoxLayout.Y_AXIS));
 
-		labelCorreo = new JLabel("Correo Electronico");
-		labelPeso = new JLabel("Peso");
-		labelIMC = new JLabel("IMC");
-
+		labelCorreo = new JLabel(p.getcorreoElectronico());
+		labelPeso = new JLabel(Double.toString(p.getPeso()));
+		labelIMC = new JLabel(""); //falta imc
+		labelAleg = new JLabel("ALERGIAS");
+		JScrollPane paneAlergias = new JScrollPane();
+		
+		
 		panelColum3.add(labelEdad);
 		panelColum3.add(labelSexo);
 		panelColum3.add(labelAltura);
+		panelColum3.add(labelAleg);
+		panelColum3.add(paneAlergias);
+
 
 		panel1.add(panelColum3);
 
 		this.add(panel1);
+		
 		this.setVisible(true);
 		this.setSize(800, 800);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
