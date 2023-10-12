8d1a46e210cded07ec0573fd3973d410a25ee430
Mikel Garduno <mikel.garduno@opendeusto.es>
2023-10-12 02:08:25 +0200

    He creado la ventana Logeo y Registro con el Tabbed pane, tambien he modificado la principal para que primero se abra esta. con pulsar el botón de iniciar sesión vale para que la ventana de resumen se abra. Falta meter en el futuro las funcionalidades, pero mañana acabo la de registro y dejo algo mejor la de inicio de sesión.

--- HealthyAndFit/src/main/Principal.java
+++ HealthyAndFit/src/main/Principal.java
@@ -1,9 +1,10 @@
 package main;
 
+import gui.VentanaLogeoRegistro;
 import gui.VentanaResumen;
 
 public class Principal {
 	public static void main(String[] args) {
-		new VentanaResumen();
+		new VentanaLogeoRegistro();
 	}
 }
