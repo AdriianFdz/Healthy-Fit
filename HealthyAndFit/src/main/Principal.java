0620f1073e175d498c2b4d7ae798013ce42ad45c
Adrian Fernandez <adrian.f@opendeusto.es>
2023-10-12 17:03:14 +0200

    Actualizacion de la ventana.
    - Alertas y foto de perfil añadida
    - Actualizada funcion vasos de agua
    - Implementacion con las clases

--- HealthyAndFit/src/main/Principal.java
+++ HealthyAndFit/src/main/Principal.java
@@ -1,11 +1,16 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.Date;
 
+import javax.swing.ImageIcon;
 import javax.swing.SwingUtilities;
 
-import domain.Preferencia;
-import domain.Sexo;
+import domain.Dieta;
+import domain.TipoAlergias;
+import domain.TipoEnfermedades;
+import domain.TipoPreferencia;
+import domain.TipoSexo;
 import domain.Usuario;
 import gui.VentanaResumen;
 
@@ -13,8 +18,7 @@ public class Principal {
 	
 	Usuario persona;
 	public static void main(String[] args) {
-		
-		Usuario p = new Usuario("Juan", "juan_mart", "Martinez", "Perez", new Date(), Sexo.HOMBRE, 1.73, 75, null, "juan@gmail.com", null, 13, Preferencia.NINGUNA);
+		Usuario p = new Usuario("Juan", "juan_perez", "Perez", "Carbon", new Date(), TipoSexo.HOMBRE, 1.75, 75, new ArrayList<TipoAlergias>(), "juan@gmail.com", new ArrayList<TipoEnfermedades>(), TipoPreferencia.NINGUNA, 12300, 3, "Ninguno", 300, new Date(), 13000, new Dieta().getNombre(), 3, "asd", new ImageIcon(System.getProperty("user.dir")+"\\resources\\images\\foto.png"));
 		SwingUtilities.invokeLater(()-> new VentanaResumen(p));
 	}
 }
