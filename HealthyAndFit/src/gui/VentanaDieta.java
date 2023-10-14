49104000e76c571729154a311dfde3708723f1aa
Oier Carbón <oier.carbon@opendeusto.es>
2023-10-14 17:25:38 +0200

    Creada y diseñada la ventana Dieta.

---
+++ HealthyAndFit/src/gui/VentanaDieta.java
@@ -0,0 +1,134 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.color.ColorSpace;
+import java.util.ArrayList;
+
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+
+import domain.Dieta;
+import domain.TipoDificultad;
+
+public class VentanaDieta extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+		JPanel panel;
+		JPanel panelArriba;
+		JPanel panelArriba2;
+		JPanel panelAbajo;
+		JLabel nombre;
+		JLabel minutos;
+		JLabel dificultad;
+		JLabel kcal;
+		JLabel Labelingredientes;
+		JTextArea ingredientes;
+		JLabel labelPasos;
+		JTextArea pasos;
+		
+	
+	
+	public VentanaDieta(Dieta d) {
+		panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panelArriba = new JPanel();
+		panelArriba.setLayout(new GridLayout(4, 2, 50, 10));
+		//panelAbajo = new JPanel();
+		//panelAbajo.setLayout(new GridLayout(2,2,100,3));
+		
+		
+		nombre = new JLabel(" " + d.getNombre());
+		nombre.setFont(new Font("Tahoma", Font.BOLD, 25));
+		nombre.setBackground((Color.gray));
+		nombre.setOpaque(true);
+		minutos = new JLabel("    ⏰" + d.getTiempo() + " minutos");
+		
+		
+		if (d.getDificultad() == TipoDificultad.FACIL) {
+			dificultad = new JLabel("Dificultad: 🔥");
+		}else if (d.getDificultad() == TipoDificultad.MEDIO) {
+			dificultad = new JLabel("Dificultad: 🔥🔥");
+		}else {
+			dificultad = new JLabel("Dificultad: 🔥🔥🔥");
+		}
+		
+		kcal = new JLabel("    Kcal: " + d.getKcal());
+		Font fuenteFont = kcal.getFont();
+		
+		Labelingredientes = new JLabel("    Ingredientes:");
+		
+		ingredientes = new JTextArea();
+		ingredientes.setEditable(false);
+		for (String s : d.getIngredientes()) {
+			ingredientes.append(s);
+		}
+	
+		
+	
+		labelPasos = new JLabel("Pasos a realizar:");
+		
+		pasos = new JTextArea();
+		pasos.setEditable(false);
+		for (String s : d.getPasos()) {
+			pasos.append(s);
+		}
+		
+		JScrollPane pane = new JScrollPane();
+		pasos.add(pane);
+		ingredientes.add(pane);
+		
+		kcal.setFont(new Font(fuenteFont.getFontName(), fuenteFont.getStyle(), 20));
+		Labelingredientes.setFont(new Font(fuenteFont.getFontName(), fuenteFont.getStyle(), 20));
+		labelPasos.setFont(new Font(fuenteFont.getFontName(), fuenteFont.getStyle(), 20));
+		minutos.setFont(new Font(fuenteFont.getFontName(), fuenteFont.getStyle(), 20));
+		dificultad.setFont(new Font(fuenteFont.getFontName(), fuenteFont.getStyle(), 20));
+		
+		JLabel label = new JLabel();
+		
+		panelArriba.add(minutos);
+		panelArriba.add(labelPasos);
+		panelArriba.add(kcal);
+		panelArriba.add(pasos);
+		panelArriba.add(Labelingredientes);
+		panelArriba.add(label);
+		panelArriba.add(ingredientes);
+		
+		//panelAbajo.add(Labelingredientes);
+		//panelAbajo.add(labelPasos);
+		//panelAbajo.add(ingredientes);
+		//panelAbajo.add(pasos);
+		
+		panel.add(panelArriba,BorderLayout.NORTH);
+		//panel.add(panelAbajo);
+
+		
+		
+		this.add(nombre,BorderLayout.NORTH);
+		this.add(panel);
+		
+		
+		this.setVisible(true);
+		this.setSize(800, 800);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setTitle("Dieta");
+		
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(()-> new VentanaDieta(new Dieta("Pollo al curry", 50, TipoDificultad.FACIL, 500,new ArrayList<String>(),new ArrayList<String>() )));
+	}
+}
--- HealthyAndFit/src/main/Principal.java
+++ HealthyAndFit/src/main/Principal.java
@@ -12,6 +12,8 @@ import domain.TipoEnfermedades;
 import domain.TipoPreferencia;
 import domain.TipoSexo;
 import domain.Usuario;
+import gui.VentanaLogeoRegistro;
+import gui.VentanaPerfil;
 import gui.VentanaResumen;
 
 public class Principal {
@@ -20,5 +22,6 @@ public class Principal {
 	public static void main(String[] args) {
 		Usuario p = new Usuario("Juan", "juan_perez", "Perez", "Carbon", new Date(), TipoSexo.HOMBRE, 1.75, 75, new ArrayList<TipoAlergias>(), "juan@gmail.com", new ArrayList<TipoEnfermedades>(), TipoPreferencia.NINGUNA, 12300, 3, "Ninguno", 300, new Date(), 13000, new Dieta().getNombre(), 3, "asd", new ImageIcon("resources\\images\\foto.png"));
 		SwingUtilities.invokeLater(()-> new VentanaResumen(p));
+		new VentanaLogeoRegistro();
 	}
 }
