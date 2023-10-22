4cad1dbd1cd0e2149f5dfbc7aa0cac60e31ad80d
Adrian Fernandez <adrian.f@opendeusto.es>
2023-10-22 17:30:31 +0200

    Añadida referencia de imagen de perfil

--- HealthyAndFit/src/main/Principal.java
+++ HealthyAndFit/src/main/Principal.java
@@ -11,6 +11,7 @@ import javax.swing.SwingUtilities;
 import domain.Dieta;
 import domain.TipoAlergias;
 import domain.TipoEnfermedades;
+import domain.TipoPermiso;
 import domain.TipoPreferencia;
 import domain.TipoSexo;
 import domain.Usuario;
@@ -24,9 +25,9 @@ public class Principal {
 	
 	Usuario persona;
 	public static void main(String[] args) {
-		Usuario p = new Usuario("Juan", "juan_perez", "Perez", "Carbon", LocalDate.of(2004, 6, 10), TipoSexo.HOMBRE, 1.75, 75, new ArrayList<TipoAlergias>(), "juan@gmail.com", new ArrayList<TipoEnfermedades>(), TipoPreferencia.NINGUNA, 12300, 3, "Ninguno", 300, LocalDate.now(), 13000, new Dieta().getNombre(), 3, "asd", new ImageIcon("resources\\images\\foto.png"));
+		//Imagen sacada de www.dazn.com
+		Usuario p = new Usuario("Juan", "juan_perez", "Perez", "Carbon", LocalDate.of(2004, 6, 10), TipoSexo.HOMBRE, 1.75, 75, new ArrayList<TipoAlergias>(), "juan@gmail.com", new ArrayList<TipoEnfermedades>(), TipoPreferencia.NINGUNA, 12300, 3, "Ninguno", 300, LocalDate.now(), 13000, new Dieta().getNombre(), 3, "asd", new ImageIcon("resources\\images\\foto.png"), TipoPermiso.ADMINISTRADOR);
 		System.out.println(p.getfechaNacimiento().getYear());
 		SwingUtilities.invokeLater(()-> new VentanaEntrenamiento());
-		//new VentanaLogeoRegistro();
 	}
 }
