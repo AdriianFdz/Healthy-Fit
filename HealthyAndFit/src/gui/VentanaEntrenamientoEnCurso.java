1385bb3168c9a67180945f10728adca788375b53
Oier Carbón <oier.carbon@opendeusto.es>
2023-10-22 18:14:57 +0200

    Añadido posible cronometro a VentanaEntrenamientoEnCurso

--- HealthyAndFit/src/gui/VentanaEntrenamientoEnCurso.java
+++ HealthyAndFit/src/gui/VentanaEntrenamientoEnCurso.java
@@ -1,10 +1,141 @@
 package gui;
 
-public class VentanaEntrenamientoEnCurso {
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class VentanaEntrenamientoEnCurso extends JFrame{
+	
+	 private static final long serialVersionUID = 1L;
 
-	}
+	  
+	    private JPanel panel;
+	    private JLabel panelTiempo;
+
+	    private JPanel buttonPanel;
+	    private JButton botonStart;
+	    private JButton botonReset;
+	    private JButton botonStop;
+
+	  
+	    private byte centiseconds = 0;
+	    private byte seconds = 20;
+	    private short minutes = 0;
+
+	    private DecimalFormat timeFormatter;
+
+	    private Timer timer;
+
+	    public VentanaEntrenamientoEnCurso() {
+	        panel = new JPanel();
+	        panel.setLayout(new BorderLayout());
+
+	        panelTiempo = new JLabel();
+	        panelTiempo.setFont(new Font("Consolas", Font.PLAIN, 30));
+	        panelTiempo.setHorizontalAlignment(JLabel.CENTER);
+	        panel.add(panelTiempo);
+
+	        buttonPanel = new JPanel();
+	        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+	        botonStart = new JButton("Start");
+	        botonStart.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent e) {
+
+	                timer.start();
+
+	            }
+	        });
+	        buttonPanel.add(botonStart);
+
+	        botonReset = new JButton("Reset");
+	        botonReset.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent e) {
+
+	                timer.stop();
+
+	                centiseconds = 0;
+	                seconds = 30;
+	                minutes = 0;
+
+	                panelTiempo.setText(timeFormatter.format(minutes) + ":"
+	                        + timeFormatter.format(seconds) + "."
+	                        + timeFormatter.format(centiseconds));
+	            }
+	        });
+
+	        buttonPanel.add(botonReset);
+
+	        botonStop = new JButton("Stop");
+	        botonStop.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent e) {
+	                timer.stop();
+	            }
+	        });
+
+	        buttonPanel.add(botonStop);
+
+	        panel.add(buttonPanel, BorderLayout.SOUTH);
+
+	        timeFormatter = new DecimalFormat("00");
+
+	        timer = new Timer(10, new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                if (centiseconds > 0) {
+	                    centiseconds--;
+	                } else {
+	                    if (seconds == 0 && minutes == 0) {
+	                        timer.stop();
+	                    } else if (seconds > 0) {
+	                        seconds--;
+	                        centiseconds = 99;
+	                    } else if (minutes > 0) {
+	                        minutes--;
+	                        seconds = 59;
+	                        centiseconds = 99;
+	                    }
+	                }
+	                panelTiempo.setText(timeFormatter.format(minutes) + ":"
+	                        + timeFormatter.format(seconds) + "."
+	                        + timeFormatter.format(centiseconds));
+	            }
+	        });
+
+	        panelTiempo.setText(timeFormatter.format(minutes) + ":"
+	                + timeFormatter.format(seconds) + "."
+	                + timeFormatter.format(centiseconds));
+
+	        add(panel);
+
+	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        setLocationRelativeTo(null);
+	        setTitle("StopwatchGUI.java");
+
+	        pack();
+	        setVisible(true);
+	    }
+
+	    public static void main(String[] args) {
+	        EventQueue.invokeLater(new Runnable() {
+	            @Override
+	            public void run() {
+	             
+
+	                new VentanaEntrenamientoEnCurso();
+	            }
+	        });
+	    }
 
 }
--- HealthyAndFit/src/main/Principal.java
+++ HealthyAndFit/src/main/Principal.java
@@ -4,12 +4,14 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.swing.ImageIcon;
 import javax.swing.SwingUtilities;
 
 import domain.Dieta;
 import domain.TipoAlergias;
+import domain.TipoDificultad;
 import domain.TipoEnfermedades;
 import domain.TipoPreferencia;
 import domain.TipoSexo;
@@ -24,9 +26,11 @@ public class Principal {
 	
 	Usuario persona;
 	public static void main(String[] args) {
-		Usuario p = new Usuario("Juan", "juan_perez", "Perez", "Carbon", LocalDate.of(2004, 6, 10), TipoSexo.HOMBRE, 1.75, 75, new ArrayList<TipoAlergias>(), "juan@gmail.com", new ArrayList<TipoEnfermedades>(), TipoPreferencia.NINGUNA, 12300, 3, "Ninguno", 300, LocalDate.now(), 13000, new Dieta().getNombre(), 3, "asd", new ImageIcon("resources\\images\\foto.png"));
-		System.out.println(p.getfechaNacimiento().getYear());
+		//Usuario p = new Usuario("Juan", "juan_perez", "Perez", "Carbon", LocalDate.of(2004, 6, 10), TipoSexo.HOMBRE, 1.75, 75, new ArrayList<TipoAlergias>(), "juan@gmail.com", new ArrayList<TipoEnfermedades>(), TipoPreferencia.NINGUNA, 12300, 3, "Ninguno", 300, LocalDate.now(), 13000, new Dieta().getNombre(), 3, "asd", new ImageIcon("resources\\images\\foto.png"));
+		//System.out.println(p.getfechaNacimiento().getYear());
 		SwingUtilities.invokeLater(()-> new VentanaEntrenamiento());
-		//new VentanaLogeoRegistro();
+		new VentanaLogeoRegistro();
+		new VentanaDieta(new Dieta("Pollo", 40, TipoDificultad.FACIL, 300, new ArrayList<String>(), new ArrayList<String>()  ));
+		
 	}
 }
