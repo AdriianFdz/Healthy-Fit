de3d79c07af139f407fe071b6ed316e28efa6c3d
Iker Alvis <iker.a@opendeusto.es>
2023-10-29 00:24:27 +0200

    Creacion de Listeners, arreglo foto, seguir con elementos de la ventana

--- HealthyAndFit/src/gui/VentanaPerfil.java
+++ HealthyAndFit/src/gui/VentanaPerfil.java
@@ -1,8 +1,12 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.time.LocalDate;
 import java.time.Period;
 
@@ -14,9 +18,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
 
 import domain.TipoAlergias;
 import domain.TipoEnfermedades;
+import domain.TipoPermiso;
 import domain.TipoPreferencia;
 import domain.Usuario;
 
@@ -53,15 +59,16 @@ public class VentanaPerfil extends JFrame{
 		panelColum1 = new JPanel();
 		panelColum1.setLayout(new BoxLayout(panelColum1, BoxLayout.Y_AXIS));
 
-		ImageIcon imageIcon = new ImageIcon("");
-		JLabel fotoUsuario = new JLabel(imageIcon);
+		Image foto = p.getFoto().getImage().getScaledInstance(300, 300, Image.SCALE_SMOOTH);
+		JLabel fotoUsuario = new JLabel(new ImageIcon(foto));
+		fotoUsuario.setPreferredSize(new Dimension(320, 320));
 		panelColum1.add(fotoUsuario);
 
 		labelNombre = new JLabel(p.getNombre());
 		labelApellido1 = new JLabel(p.getApellido1());
 		labelApellido2 = new JLabel(p.getApellido2());
-		labelFechaNac = new JLabel(p.getfechaNacimiento().toString()); //esto no se como seria con lo de DATE
-		labelTipoU = new JLabel(""); //donde dice q tipo de usuario es
+		labelFechaNac = new JLabel(p.getfechaNacimiento().toString()); // esto no se como seria con lo de DATE
+		labelTipoU = new JLabel(p.getPermiso().name());
 
 		panelColum1.add(labelNombre);
 		panelColum1.add(labelApellido1);
@@ -69,43 +76,49 @@ public class VentanaPerfil extends JFrame{
 		panelColum1.add(labelTipoU);
 		panelColum1.add(labelFechaNac);
 
+		// Botones
 		JButton modificarBot = new JButton("MODIFICAR DATOS");
 		JButton accesoPanelBot = new JButton("ACEESO PANEL");
-		
-		JButton volverBot =  new JButton("VOLVER");
+
+		JButton volverBot = new JButton("VOLVER");
+		JButton botonCerSesion = new JButton("CERRAR SESION");
 
 		panelColum1.add(modificarBot);
 		panelColum1.add(accesoPanelBot);
-		
-		panel2.add(volverBot, BorderLayout.WEST);
-		
+
+		// Definir la condicion del boton Acceso
+		if (p.getPermiso() != TipoPermiso.ADMINISTRADOR) {
+			accesoPanelBot.setVisible(false);
+		}
+
 		panel1.add(panelColum1);
 
-		
+		panel2.add(volverBot, BorderLayout.WEST);
+		panel2.add(botonCerSesion, BorderLayout.EAST);
+
 		panelColum2 = new JPanel();
 		panelColum2.setLayout(new BoxLayout(panelColum2, BoxLayout.Y_AXIS));
 
-		
 		int edad = Period.between(p.getfechaNacimiento(), LocalDate.now()).getYears();
-					
+
 		labelEdad = new JLabel(edad + "");
 		labelSexo = new JLabel(p.getSexo().name());
 		labelAltura = new JLabel(Double.toString(p.getAltura()));
 		labelEnfer = new JLabel("Enfermedades");
 
 		JScrollPane paneEnfermedades = new JScrollPane();
-		for (TipoEnfermedades enfermedad: p.getEnfermedades()) {
+		for (TipoEnfermedades enfermedad : p.getEnfermedades()) {
 			JLabel label1 = new JLabel(enfermedad.name());
 			paneEnfermedades.add(label1);
 		}
 
-
 		labelPrefAli = new JLabel("PREFERENCIAS ALIMENTICIAS");
 		JComboBox<TipoPreferencia> preferenciasComboBox = new JComboBox<TipoPreferencia>();
-		//for (TipoPreferencia preferencia : p.getpreferenciaAlimenticia() ) {
-		//	preferenciasComboBox.addItem(preferencia);
-		//}
-		
+		preferenciasComboBox.addItem(p.getpreferenciaAlimenticia());
+		// for (TipoPreferencia preferencia : p.getpreferenciaAlimenticia() ) {
+		// preferenciasComboBox.addItem(preferencia);
+		// }
+
 		panelColum2.add(labelEdad);
 		panelColum2.add(labelSexo);
 		panelColum2.add(labelAltura);
@@ -113,42 +126,67 @@ public class VentanaPerfil extends JFrame{
 		panelColum2.add(paneEnfermedades);
 		panelColum2.add(new Label("PREFERENCIA ALIMENTICIA"));
 		panelColum2.add(preferenciasComboBox);
-		
+
 		panel1.add(panelColum2);
 
+		
 		panelColum3 = new JPanel();
 		panelColum3.setLayout(new BoxLayout(panelColum3, BoxLayout.Y_AXIS));
 
 		labelCorreo = new JLabel(p.getcorreoElectronico());
 		labelPeso = new JLabel(Double.toString(p.getPeso()));
-		labelIMC = new JLabel("IMC"); //falta imc
+		labelIMC = new JLabel(Double.toString(p.getImc()));
 		labelAleg = new JLabel("ALERGIAS");
-		
+
 		JScrollPane paneAlergias = new JScrollPane();
 		for (TipoAlergias alergia : p.getAlergias()) {
 			JLabel label = new JLabel(alergia.name());
 			paneAlergias.add(label);
 		}
-		JButton botonCerSesion = new JButton("CERRAR SESION");
-		
-		
+
 		panelColum3.add(labelCorreo);
 		panelColum3.add(labelPeso);
 		panelColum3.add(labelIMC);
 		panelColum3.add(labelAleg);
 		panelColum3.add(paneAlergias);
-		
-		panel2.add(botonCerSesion, BorderLayout.WEST);
 
 		panel1.add(panelColum3);
+		
+		// LISTENERS
+		modificarBot.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		accesoPanelBot.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		botonCerSesion.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				SwingUtilities.invokeLater(() -> new VentanaLogeoRegistro());
+				dispose();
+			}
+		});
 
 		this.add(panel1);
-	
+
 		this.setVisible(true);
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setTitle("Healthy & Fit");
-		this.setSize(1920,1080);
+		this.setSize(1920, 1080);
 	}
 	
 }
