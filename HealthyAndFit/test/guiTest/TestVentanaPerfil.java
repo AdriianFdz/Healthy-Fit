c00c0e3baddde1ae404271530b1edd609c0b5d22
Adrian Fernandez <adrian.f@opendeusto.es>
2023-11-19 19:26:06 +0100

    Test ventana perfil realizado

--- HealthyAndFit/test/guiTest/TestVentanaPerfil.java
+++ HealthyAndFit/test/guiTest/TestVentanaPerfil.java
@@ -1,5 +1,40 @@
 package guiTest;
 
-public class TestVentanaPerfil {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+import javax.swing.ImageIcon;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import domain.Dieta;
+import domain.TipoAlergias;
+import domain.TipoEnfermedades;
+import domain.TipoPermiso;
+import domain.TipoPreferencia;
+import domain.TipoSexo;
+import domain.Usuario;
+import gui.VentanaPerfil;
+
+public class TestVentanaPerfil {
+	Usuario usuario;
+	Usuario usuarioSinPermisos;
+	VentanaPerfil ventana;
+	VentanaPerfil ventana2;
+	@Before
+	public void setUp() {
+		usuario = new Usuario("Juan", "juan_perez", "Perez", "Carbon", LocalDate.of(2004, 6, 10), TipoSexo.HOMBRE, 1.75, 75, new ArrayList<TipoAlergias>(Arrays.asList(TipoAlergias.HUEVOS)), "juan@gmail.com", new ArrayList<TipoEnfermedades>(Arrays.asList(TipoEnfermedades.CARDIOVASCULARES)), TipoPreferencia.NINGUNA, 12300, 3, "Ninguno", 300, LocalDate.now(), 13000, new Dieta().getNombre(), 3, "juan", new ImageIcon("resources\\images\\foto.png"), TipoPermiso.ADMINISTRADOR);
+		usuarioSinPermisos = new Usuario("Juan", "juan_perez", "Perez", "Carbon", LocalDate.of(2004, 6, 10), TipoSexo.HOMBRE, 1.75, 75, new ArrayList<TipoAlergias>(Arrays.asList(TipoAlergias.HUEVOS)), "juan@gmail.com", new ArrayList<TipoEnfermedades>(Arrays.asList(TipoEnfermedades.CARDIOVASCULARES)), TipoPreferencia.NINGUNA, 12300, 3, "Ninguno", 300, LocalDate.now(), 13000, new Dieta().getNombre(), 3, "juan", new ImageIcon("resources\\images\\foto.png"), TipoPermiso.USUARIO);
+	}
+	
+	@Test
+	public void comprobarVentanaConPermisos() {
+		ventana = new VentanaPerfil(usuario);
+	}
+	@Test
+	public void comprobarVentanaSinPermisos() {
+		ventana2 = new VentanaPerfil(usuarioSinPermisos);	
+	}
 }
